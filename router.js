6f84a6312f6cb92082892fc6656a64b3a029e0a2
Odysseas Moschidis <odysseasmoschidis@gmail.com>
2020-06-06 17:13:28 +0300

    fixxed tab length, and minnimized workout stracture

--- router.js
+++ router.js
@@ -3,49 +3,53 @@ const router = express.Router()
 const db = require('./db')
 
 router.get('/', function(req, res) {
-    res.send(`
-    <form action="/insertTestWorkout" method="POST">
-        <p>Who is inserting the test workout?</p>
-        <input name="name" autocomplete="off">
-        <button>Submit</button>
-    </form>
-    `)
+  res.send(`
+  <form action="/insertTestWorkout" method="POST">
+    <p>Who is inserting the test workout?</p>
+    <input name="name" autocomplete="off">
+    <button>Submit</button>
+  </form>
+  `)
 })
 
 router.post('/insertTestWorkout', function(req, res) {
 
-    let testWorkout = {
-        title: "Basic Calisthenics", 
-        version: "1.0",
-        groups: [{
-            exercise: "push ups",
-            sets: 5,
-            reps: 10
-        },
-        {
-            exercise: "pull ups",
-            sets: 5,
-            reps: 6
-        },
-        {
-            supersets: 4,
-            exercises: [{
-                exercise: "ab crunches",
-                sets: 1,
-                reps: 25
-            },
-            {
-                exercise: "prone back extensions",
-                sets: 1,
-                reps: 30
-            }]
-        }]
-    }
+  let testWorkout = {
+    title: "Basic Calisthenics",
+    description: "This is a basic, full body, calisthenics workout for begginer to intermidiate athletes, it covers the basics with a good amount of volume.",
+    version: "1.0",
+    type: "normal",
+    groups: [{
+      sets: 5,
+      setExercises: [{
+        exercise: "push ups",
+        reps: 10
+      }]
+    },
+    {
+      sets: 5,
+      setExercises: [{
+        exercise: "pull ups",
+        reps: 6
+      }]
+    },
+    {
+      sets: 4,
+      setExercises: [{
+        exercise: "ab crunches",
+        reps: 25
+      },
+      {
+        exercise: "prone back extensions",
+        reps: 30
+      }]
+    }]
+  }
     
-    db.collection('workouts').insertOne(testWorkout, () => {
-        console.log("Test workout inserted.")
-        res.send("Test workout inserted by " + req.body.name + " .")
-    })
+  db.collection('workouts').insertOne(testWorkout, () => {
+    console.log("Test workout inserted.")
+    res.send("Test workout inserted by " + req.body.name + " .")
+  })
 })
 
 module.exports = router
