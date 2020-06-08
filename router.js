b4ae91b12af862816b631f51ce3861fdb1e13e19
Odysseas Moschidis <odysseasmoschidis@gmail.com>
2020-06-08 21:00:22 +0300

    added basic submit workout form

--- router.js
+++ router.js
@@ -12,39 +12,39 @@ router.get('/', function(req, res) {
   `)
 })
 
-router.post('/insertTestWorkout', function(req, res) {
-
-  let testWorkout = {
-    title: "Basic Calisthenics",
-    description: "This is a basic, full body, calisthenics workout for begginer to intermidiate athletes, it covers the basics with a good amount of volume.",
-    version: "1.0",
-    type: "normal",
-    groups: [{
-      sets: 5,
-      setExercises: [{
-        exercise: "push ups",
-        reps: 10
-      }]
-    },
-    {
-      sets: 5,
-      setExercises: [{
-        exercise: "pull ups",
-        reps: 6
-      }]
-    },
-    {
-      sets: 4,
-      setExercises: [{
-        exercise: "ab crunches",
-        reps: 25
-      },
-      {
-        exercise: "prone back extensions",
-        reps: 30
-      }]
+let testWorkout = {
+  title: "Basic Calisthenics",
+  description: "This is a basic, full body, calisthenics workout for begginer to intermidiate athletes, it covers the basics with a good amount of volume.",
+  version: "1.0",
+  type: "normal",
+  groups: [{
+    sets: 5,
+    setExercises: [{
+      exercise: "push ups",
+      reps: 10
     }]
-  }
+  },
+  {
+    sets: 5,
+    setExercises: [{
+      exercise: "pull ups",
+      reps: 6
+    }]
+  },
+  {
+    sets: 4,
+    setExercises: [{
+      exercise: "ab crunches",
+      reps: 25
+    },
+    {
+      exercise: "prone back extensions",
+      reps: 30
+    }]
+  }]
+}
+
+router.post('/insertTestWorkout', function(req, res) {
     
   db.collection('workouts').insertOne(testWorkout, () => {
     console.log("Test workout inserted.")
